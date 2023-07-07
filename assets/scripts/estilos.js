26ba811446326afaf09e46777787e9027ea829d4
giperuzzo <gisleneperuzzo@gmail.com>
2023-07-07 11:52:09 -0300

    estilização

---
+++ assets/scripts/estilos.js
@@ -0,0 +1,8 @@
+const styles = document.createElement('link');
+
+styles.setAttribute('rel', 'stylesheet');
+styles.setAttribute('href', './assets/styles/style.css'),
+
+/* link rel = stysheet "href=" ./assets/styles/style.css"> */
+
+document.head.appendChild(styles);
